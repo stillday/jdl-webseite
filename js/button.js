15b30195794ffc5cfbab230acc940997dff94290
Jan Drenkhahn <jan-dennisd@outlook.at>
2017-10-03 21:35:55 +0200

    inlinePopup

---
+++ js/button.js
@@ -0,0 +1,13 @@
+// Example 1: From an element in DOM
+$('.open-popup-link').magnificPopup({
+    type:'inline',
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+
+$('button').magnificPopup({
+    items: {
+        src: '<div class="white-popup">Dynamically created popup</div>',
+        type: 'inline'
+    },
+    closeBtnInside: true
+  });
